9c2c1e2c5dc01fc84b1d6e687136bf03cd83df70
岡本泰輔 <base225001@gmail.com>
2023-03-01 13:19:35 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.56.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "botapeer-terraform-state"
+    region  = "ap-northeast-1"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "botapeer-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
