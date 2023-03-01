f114e0ae9a902d5ba80864c33956e0f9cd336c15
岡本泰輔 <base225001@gmail.com>
2023-03-01 15:54:52 +0900

    vpc名称変更

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,19 @@
-resource "aws_vpc" "vpc1" {
+resource "aws_vpc" "botapeer_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "vpc1"
+    Name = "botapeer-vpc"
   }
 }
 
 resource "aws_internet_gateway" "botapeer_igw"{
-  vpc_id = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.botapeer_vpc.id
   tags = {
     Name = "botapeer-igw"
   }
 }
 
 resource "aws_subnet" "public_1a" {
-  vpc_id = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.botapeer_vpc.id
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.0.1.0/24"
   tags = {
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_1a" {
 }
 
 resource "aws_subnet" "private_1a" {
-  vpc_id = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.botapeer_vpc.id
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.0.10.0/24"
     tags = {
