cf918e50953679e3ae99b6e4d96d6b355a965f2d
岡本泰輔 <base225001@gmail.com>
2023-03-01 14:46:17 +0900

    bucket定義変更

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.terraform_state.bucket
   versioning_configuration {
     status = "Enabled"
   }
