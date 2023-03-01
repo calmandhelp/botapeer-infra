7a5b510f0bd3998ffaec44c8edad311edf593a30
岡本泰輔 <base225001@gmail.com>
2023-03-01 15:19:19 +0900

    igw private subnet作成

--- vpc.tf
+++ vpc.tf
@@ -5,11 +5,27 @@ resource "aws_vpc" "vpc1" {
   }
 }
 
+resource "aws_internet_gateway" "botapeer_igw"{
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "botapeer-igw"
+  }
+}
+
 resource "aws_subnet" "public_1a" {
   vpc_id = aws_vpc.vpc1.id
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.0.1.0/24"
   tags = {
     Name = "botapeer-public-1a"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.vpc1.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.0.10.0/24"
+    tags = {
+    Name = "botapeer-private-1a"
   }
 }
