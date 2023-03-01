ec159fcc2b3242a46688584710ff214280682f20
岡本泰輔 <base225001@gmail.com>
2023-03-01 14:55:52 +0900

    public subnet作成

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,15 @@
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc1"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.vpc1.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "botapeer-public-1a"
+  }
 }
