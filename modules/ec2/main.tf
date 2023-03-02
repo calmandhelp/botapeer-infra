682fb18431c3630c6ee2fe227768b7aa4a2a17cf
岡本泰輔 <base225001@gmail.com>
2023-03-02 16:10:13 +0900

    ディレクトリ整理

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,23 +1,42 @@
-# resource "aws_security_group" "server-sg" {
-#   name        = "server-sg"
-#   description = "server-sg"
- 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
+# resource "aws_instance" "higa-ec2-1" {
+
+#   tags = {
+#     Name = "higa-ec2-1"
+#     Dlm  = "higa-ec2-BackUp"
 #   }
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
+
+#   ami                    = "ami-0bd6906508e74f692" # Amazon Linux 2
+#   instance_type          = "t2.micro"
+#   subnet_id              = aws_subnet.higa-Public-subnet-1a.id
+#   vpc_security_group_ids = [aws_security_group.higa-ec2-sg.id]
+#   private_ip             = "10.0.1.10"
+#   key_name               = "higa1-keypair"
+#   iam_instance_profile   = "higa_profile"
+
+#   //キャパシティー予約をなしに設定
+#   capacity_reservation_specification {
+#     capacity_reservation_preference = "none"
 #   }
-#   ingress {
-#     from_port   = 443
-#     to_port     = 443
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
+
+#   # EBSのルートボリューム設定
+#   root_block_device {
+#     // ボリュームサイズ(GiB)
+#     volume_size = 8
+#     // ボリュームタイプ
+#     volume_type = "gp3"
+#     // GP3のIOPS
+#     iops = 3000
+#     // GP3のスループット
+#     throughput = 125
+#     // EC2終了時に削除
+#     delete_on_termination = true
+
+#     // 暗号化KMS設定
+#     encrypted  = true
+#     kms_key_id = aws_kms_key.higa_KMS.key_id
+
+#     // EBSのNameタグ
+#     tags = {
+#       Name = "higa-gp3-dev-ec2"
+#     }
 #   }
-# }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,13 +5,6 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.service_name}-igw"
-  }
-}
-
 resource "aws_subnet" "public_1a" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = var.az1
@@ -28,20 +21,4 @@ resource "aws_subnet" "private_1a" {
   tags = {
     Name = "${var.service_name}-private-1a"
   }
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id            = aws_vpc.vpc.id
-  route {
-    cidr_block      = "0.0.0.0/0"
-    gateway_id      = aws_internet_gateway.igw.id
-  }
-  tags = {
-    Name = "${var.service_name}-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_rt_associate" {
-  subnet_id      = aws_subnet.public_1a.id
-  route_table_id = aws_route_table.public_rt.id
 }
