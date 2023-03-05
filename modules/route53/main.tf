1a77b8e2f28e795b51e3a934442f4c3a4020266f
岡本泰輔 <base225001@gmail.com>
2023-03-05 21:42:46 +0900

    subdomain追加

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,3 +1,37 @@
-resource "aws_route53_zone" "service_zone" {
+data aws_route53_zone service_zone {
    name = "botapeer.com"
+}
+
+resource "aws_route53_zone" "subdomain_api" {
+  name = "api.botapeer.com"
+}
+
+resource "aws_route53_zone" "subdomain_image" {
+  name = "image.botapeer.com"
+}
+
+resource "aws_route53_record" "ns_record_for_subdomain_api" {
+  name    = aws_route53_zone.subdomain_api.name
+  zone_id = data.aws_route53_zone.service_zone.id
+  records = [
+    aws_route53_zone.subdomain_api.name_servers[0],
+    aws_route53_zone.subdomain_api.name_servers[1],
+    aws_route53_zone.subdomain_api.name_servers[2],
+    aws_route53_zone.subdomain_api.name_servers[3]
+  ]
+  ttl  = 300
+  type = "NS"
+}
+
+resource "aws_route53_record" "ns_record_for_subdomain_image" {
+  name    = aws_route53_zone.subdomain_image.name
+  zone_id = data.aws_route53_zone.service_zone.id
+  records = [
+    aws_route53_zone.subdomain_image.name_servers[0],
+    aws_route53_zone.subdomain_image.name_servers[1],
+    aws_route53_zone.subdomain_image.name_servers[2],
+    aws_route53_zone.subdomain_image.name_servers[3]
+  ]
+  ttl  = 300
+  type = "NS"
 }
