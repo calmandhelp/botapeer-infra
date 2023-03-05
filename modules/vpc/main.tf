b6d3d31803f3ca9a62283bed8831cf2bbc62ec12
岡本泰輔 <base225001@gmail.com>
2023-03-05 16:47:57 +0900

    private subnet修正

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,9 +25,9 @@ resource "aws_subnet" "private_1a" {
 
 resource "aws_subnet" "private_1c" {
   vpc_id            = aws_vpc.vpc.id
-  availability_zone = var.az1
+  availability_zone = var.az2
   cidr_block        = var.cidr_private1c
   tags = {
-    Name = "${var.service_name}-private-1a"
+    Name = "${var.service_name}-private-1c"
   }
 }
